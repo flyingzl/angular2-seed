fd93fb3630c098849c8a3ddfd16c9e4a0148556a
lzhao <lzhao@tcl.com>
2016-06-01 20:36:44 +0800

    更新到angular2 rc.1

--- resouces/webpack.prod.config.js
+++ resouces/webpack.prod.config.js
@@ -1,51 +1,35 @@
-var path = require("path"),
-    webpack = require("webpack"),
-    proxy = require("./proxy");
+var path = require('path'),
+    webpack = require('webpack');
 
 var SRC_PATH = path.join(__dirname, 'src'),
-    DIST_PATH = path.join(__dirname, '../static'),
-    FILE_HASH_TAG = '_[hash:5]',
-    CHUNK_FILE_HASH_TAG = '_[chunkhash:5]';
-
-var HtmlWebpackPlugin = require('html-webpack-plugin'),
-    ExtractTextPlugin = require("extract-text-webpack-plugin");
+    DIST_PATH = path.join(__dirname, '../static');
 
 
 var config = {
+    context: SRC_PATH,
     entry: {
-        app: path.join(SRC_PATH, 'app.js'),
+        app: './app/app.js',
+        form: path.join(SRC_PATH, 'app/form.js'),
+        router: path.join(SRC_PATH, 'app/router.js'),
         vendors: [
-            'lodash',
-            'classnames',
-            'querystring',
-            'react',
-            'react-dom',
-            'react-redux',
-            'react-router',
-            'react-router-redux',
-            'react-bootstrap',
-            'redux',
-            'redux-form',
-            'redux-actions',
-            'redux-async',
-            'redux-thunk',
-            // For echarts
-            'echarts/lib/echarts',
-            'echarts/lib/chart/bar'
-        ],
-        commons: path.join(SRC_PATH, 'layouts/css/common.less')
+            'babel-polyfill',
+            'zone.js/dist/zone',
+            '@angular/core',
+            '@angular/platform-browser-dynamic',
+            '@angular/common',
+            '@angular/router'
+        ]
     },
 
     resolve: {
         alias: {},
-        extensions: ["", ".less", ".css", ".js", ".json"]
+        extensions: ['', '.less', '.css', '.js', '.json']
     },
 
     output: {
         path: DIST_PATH,
-        publicPath: '',
-        filename: `js/[name]${FILE_HASH_TAG}.js`,
-        chunkFilename: `js/[name]${CHUNK_FILE_HASH_TAG}.js`
+        publicPath: '/static/',
+        filename: 'js/[name].js'
     },
 
     clearBeforeBuild: true,
@@ -53,27 +37,12 @@ var config = {
     plugins: [
         new webpack.optimize.CommonsChunkPlugin(
             'vendors',
-            'js/vendors_20160510.js', // vendor date
+            'js/vendors.js', // vendor date
             Infinity
         ),
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production'),
-            '__DEV__': false
-        }),
-
-        new ExtractTextPlugin(`css/[name]${FILE_HASH_TAG}.css`, { allChunks: true }),
-
-        new webpack.optimize.UglifyJsPlugin({
-            comments: false,
-            warnings: false
-        }),
-
-        new HtmlWebpackPlugin({
-            inject: false,
-            filename: '../index.jsp',
-            template: path.join(SRC_PATH, 'index.jsp'),
-            chunks: ['commons', 'vendors', 'app'],
-            context:"<%=request.getContextPath()%>/static"
+            'process.env.NODE_ENV': JSON.stringify('development'),
+            '__DEV__': true
         })
 
     ],
@@ -82,54 +51,56 @@ var config = {
         noParse: [],
 
         loaders: [{
-                test: /\.js$/,
-                loader: "babel",
-                query: {
-                    plugins: ['transform-runtime'],
-                    presets: ['es2015', 'react','stage-0']
-                },
-                include: SRC_PATH,
-                exclude: /node_modules/
+            test: /\.js$/,
+            loader: 'babel',
+            query: {
+                plugins: ['angular2-annotations','babel-plugin-transform-decorators-legacy'],
+                presets: ['es2015', 'angular2','stage-0']
             },
+
+            exclude: /node_modules/
+        },
 
             {
                 test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style", "css!autoprefixer", {
-                    publicPath: "../"
-                }),
+                loader: 'style!css!autoprefixer!less',
                 exclude: /node_modules/
             },
 
             {
                 test: /\.less$/,
-                loader: ExtractTextPlugin.extract("style", "css!autoprefixer!less", {
-                    publicPath: "../"
-                }),
+                loader: 'style!css!autoprefixer!less',
                 exclude: /node_modules/
             },
 
             {
                 test: /\.(png|jpg|gif)$/,
-                loader: "url",
+                loader: 'url',
                 query: {
                     limit: 8192,
-                    name: `imgs/[name]${FILE_HASH_TAG}.[ext]`
+                    name: 'imgs/[name].[ext]'
                 }
             },
 
             {
                 test: /\.(eot|woff|woff2|ttf|svg)((\?|\#)[\?\#\w\d_-]+)?$/,
-                loader: "url",
+                loader: 'url',
                 query: {
                     limit: 100,
                     name: 'fonts/[name].[ext]'
                 }
+            },
+
+            {
+                test: /\.html$/,
+                loader: "file?name=[name].[ext]",
+                exclude: /templates/
             }
         ]
     }
 };
 
 
-console.log("initializing webpack production build....");
+console.log('initializing webpack developent build....');
 
 module.exports = config;
